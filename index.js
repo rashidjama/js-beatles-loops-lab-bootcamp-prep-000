b3857c3465877d5a65f5c3ace913f6438a0a8927
Rashid Jama <rashidjama5@gmail.com>
2019-12-21 06:24:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let briefIntro = [];
 function theBeatlesPlay(musicians, instruments) {
   for(let i=0; i<musicians.length; i++) {
     for(let j=0; j<instruments.length; j++) {
-        briefIntro.push(`${musicians[i]} plays ${instruments[i]}.`)
     }
+    briefIntro.push(`${musicians[i]} plays ${instruments[i]}.`)
   }
   return briefIntro;
 }
